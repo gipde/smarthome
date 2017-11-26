03e26579d9378d6792c097f7d0a9c75524f34432
Werner Schneider <werner@schneidernet.com>
2017-11-26 20:52:34 +0100

    Login working

--- app/DB.go
+++ app/DB.go
@@ -1,110 +1,36 @@
 package app
 
 import (
-	"database/sql"
-	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	// "github.com/revel/revel"
 	"golang.org/x/crypto/bcrypt"
-	//	"reflect"
-	"github.com/revel/revel"
-	"strconv"
 )
 
-// type MyDB sql.DB
+type User struct {
+	ID       string `gorm:"primary_key"`
+	Name     string
+	Password []byte
+}
 
-// var DB *MyDB
-var DB *sql.DB
+var Db *gorm.DB
 
 func InitDB() {
-	driver := revel.Config.StringDefault("db.driver", "sqlite3")
-	connect_string := revel.Config.StringDefault("db.connect", "file::memory:?mode=memory&cache=shared")
 
-	revel.AppLog.Info("Starting DB Connection " + driver)
+	DbInt, err := gorm.Open("sqlite3", "$HOME/tmp/test.db")
 
-	var err error
-	DB, err = sql.Open(driver, connect_string)
 	if err != nil {
-		revel.INFO.Println("DB Error", err)
+		panic("failed to connect database")
 	}
+	// defer func() {
+	// 	revel.AppLog.Info("We Close the DB")
+	// 	Db.Close()
+	// }()
 
-	revel.AppLog.Info(strconv.FormatInt(int64(DB.Stats().OpenConnections), 10))
+	Db = DbInt.Debug().AutoMigrate(&User{})
 
-	fmt.Println(DB)
-	//revel.AppLog.Info(reflect.TypeOf(DB))
-
-	revel.INFO.Println("DB Connected")
-
-	initDB()
-}
-
-// func (db *MyDB) String() string {
-// 	return "Hello"
-// }
-
-// func GetDB() *sql.DB {
-// 	fmt.Println("from getter")
-// 	fmt.Println(DB)
-// 	return DB
-// }
-
-func initDB() {
-	exec("create table user(id varchar(10), name varchar(40), password varchar(20));")
-
-	pw, _ := bcrypt.GenerateFromPassword([]byte("geheim"), bcrypt.DefaultCost)
-	exec("insert into user values($1,$2,$3)", "werner", "Werner Schneider", pw)
-	exec("insert into tbl1 values('zwei',2);")
-}
-
-func exec(q string, args ...interface{}) {
-	revel.AppLog.Info("Executing DDL " + q)
-	result, err := DB.Exec(q, args)
-	if err != nil {
-		revel.AppLog.Fatal(err.Error())
-	}
-	rows, _ := result.RowsAffected()
-	lastInsertID, _ := result.LastInsertId()
-	revel.AppLog.Info(fmt.Sprintf("affected: %d, last ID: %d ", rows, lastInsertID))
-}
-
-func doSQL(q string) *sql.Rows {
-	revel.AppLog.Info("Query " + q)
-	rows, err := DB.Query(q)
-	//defer rows.Close()
-	if err != nil {
-		revel.AppLog.Error(err.Error())
-	}
-	fmt.Println(rows)
-	return rows
-}
-
-func printResult(rows *sql.Rows) {
-	var col []*sql.ColumnType
-	revel.AppLog.Info("loop over rows")
-	fmt.Println(rows)
-	fmt.Println(&rows)
-	firstResult := true
-	for rows.Next() {
-		if firstResult {
-			firstResult = false
-			col, _ = rows.ColumnTypes()
-			for _, c := range col {
-				revel.AppLog.Info("HEAD: " + c.Name())
-				revel.AppLog.Info("HEAD: " + c.ScanType().String())
-				revel.AppLog.Info("HEAD: " + c.DatabaseTypeName())
-				var value interface{}
-				rows.Scan(&value)
-				fmt.Println("value: ", value)
-			}
-
-			// fmt.Printf(col.Name())
-			// Columns()
-			// revel.AppLog.Info("HEAD: " + strings.Join(col, ","))
-		}
-
-		//revel.AppLog.Info("TableName: " + name)
-		// revel.AppLog.Info("Columns: Start")
-		// revel.AppLog.Info("Columns: Ende")
-	}
-	rows.Close()
-	revel.AppLog.Info(fmt.Sprintf("Columns: %d", len(col)))
+	user := User{Name: "Werner Schneider", ID: "werner"}
+	user.Password, _ = bcrypt.GenerateFromPassword([]byte("starten"), bcrypt.DefaultCost)
+	Db.Create(user)
 
 }
--- app/controllers/main.go
+++ app/controllers/main.go
@@ -1,13 +1,9 @@
 package controllers
 
 import (
-	// "database/sql"
-	//	"strings"
-
-	// "fmt"
-
 	"github.com/revel/revel"
-	// "schneidernet/smarthome/app"
+	"golang.org/x/crypto/bcrypt"
+	"schneidernet/smarthome/app"
 	"schneidernet/smarthome/app/routes"
 	"strconv"
 )
@@ -18,17 +14,19 @@ type Main struct {
 
 func (c Main) Index() revel.Result {
 	c.Log.Info("Starte Loginseite")
+	c.Log.Info(c.Session["logged_in"])
+	if c.Session["logged_in"] == "true" {
+		return c.Redirect(routes.Main.Dashboard())
+	}
 	return c.Render()
+
 }
 
 func (c Main) Dashboard() revel.Result {
-	if c.Session["key"] != "hello" {
+	if c.Session["logged_in"] != "true" {
 		c.Flash.Error("not logged in")
 		return c.Redirect(routes.Main.Index())
 	}
-
-	printResult(doSQL("SELECT * from tbl1;"))
-	printResult(doSQL("SELECT name FROM sqlite_master WHERE type='table';"))
 
 	return c.Render()
 }
@@ -39,18 +37,28 @@ func (c Main) Login(username, password string, remember bool) revel.Result {
 	c.Log.Info("User: " + username)
 	c.Log.Info("pass: " + password)
 	c.Log.Info("remember: " + strconv.FormatBool(remember))
-	if username != "hello" {
-		c.Flash.Error("Login failed")
+
+	var dbUsr app.User
+	app.Db.First(&dbUsr, "id=?", username)
+
+	if username != dbUsr.ID {
+		c.Flash.Error("User not found")
 		return c.Redirect(routes.Main.Index())
 	}
-	c.Session["key"] = "hello"
 
+	err := bcrypt.CompareHashAndPassword(dbUsr.Password, []byte(password))
+	if err != nil {
+		c.Flash.Error("Password false")
+		return c.Redirect(routes.Main.Index())
+	}
+
+	c.Session["logged_in"] = "true"
 	return c.Redirect(routes.Main.Dashboard())
 
 }
 
 // Logout API
 func (c Main) Logout() revel.Result {
-	c.Session["key"] = ""
+	delete(c.Session, "logged_in")
 	return c.Redirect(routes.Main.Index())
 }
