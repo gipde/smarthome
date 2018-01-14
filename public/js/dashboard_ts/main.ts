ec29083f9203a1e9a799bafdf2da0fd990770df4
Werner Schneider <werner@schneidernet.com>
2018-01-14 08:50:32 +0100

    ts umbau zu class

--- public/js/dashboard_ts/main.ts
+++ public/js/dashboard_ts/main.ts
@@ -1,122 +1,158 @@
+
 /*
 embed in Dashboard.html
-<script src="{{.contextRoot}}/public/js/dashboard_ts/main.js"></script> 
+<script src="{{.contextRoot}}/public/js/dashboard_ts/main.js"></script>
+
+install types
+npm install --save @types/jquery@2
+npm install --save @types/reconnectingwebsocket
 */
 
-declare var ReconnectingWebSocket: any
-declare var $: any
+ declare var ReconnectingWebSocket: any
+
 
 const xpath = ""
 const wspath = "ws://localhost:9000" + xpath
-enum DeviceType { Socket = 0, Switch = 1, Bulb = 2, Thermostat = 3 }
-const PingInterval:number=10
+
+const PingInterval: number = 300
+const ON = "ON"
+const SWITCHED_ON = "switchedOn"
+enum DeviceType {
+    Socket = 0,
+    Switch = 1,
+    Bulb = 2,
+    Thermostat = 3
+}
+enum Action {
+    Ping = "Ping",
+    StateResponse = "StateResponse",
+    StateUpdate = "StateUpdate",
+    RequestState = "RequestState",
+    FlipState = "FlipState",
+}
+
 
 interface Device {
-    id: string;
-    connected?: boolean;
-    devicetype?: DeviceType;
+    id: string
+    connected?: boolean
+    devicetype?: DeviceType
 }
+
 interface DevProto {
-    action: string;
-    device?: Device;
-    payload?: string;
+    action: Action
+    device?: Device
+    payload?: string
 }
 
+class Dashboard {
 
+    socket: any
+    timer: number
 
-
-function send(action: string ,id:string): void {
-    socket.send(JSON.stringify({
-        action: action,
-        device: { id: id}
-    } as DevProto))
-}
-
-function setDevHdl(): void {
-    $(".circle").each(function () { send("RequestState",$(this)[0].id) })
-    $(".circle").click(function () { send("FlipState",$(this)[0].id) })
-}
-
-function drawSwitchable(incoming: DevProto, selector: any, part: string): void {
-    if (incoming.payload == "ON") {
-        selector.html("<img src=/public/img/" + part + "_on.svg width=30 height=30/>")
-        selector.addClass("switchedOn")
-    } else {
-        selector.html("<img src=/public/img/" + part + "_off.svg width=30 height=30/>")
-        selector.removeClass("switchedOn")
+    drawSwitchable(incoming: DevProto, selector: any, part: string): void {
+        if (incoming.payload == ON) {
+            selector.html("<img src=/public/img/" + part + "_on.svg width=30 height=30/>")
+            selector.addClass(SWITCHED_ON)
+        } else {
+            selector.html("<img src=/public/img/" + part + "_off.svg width=30 height=30/>")
+            selector.removeClass(SWITCHED_ON)
+        }
     }
-}
 
-function hdlIncoming(event: any): void {
-    let data: DevProto = JSON.parse(event.data)
-    let sel = $("#" + data.device.id)
-    switch (data.action) {
-        case "StateResponse":
-        case "StateUpdate": {
-            if (data.device.connected) {
-                sel.addClass("connected")
+    getIncomingHdl() {
+        return (event: any) => {
+            let data: DevProto = JSON.parse(event.data)
+            let sel = $("#" + data.device.id)
+            switch (data.action) {
+                case Action.StateResponse:
+                case Action.StateUpdate: {
+                    if (data.device.connected) {
+                        sel.addClass("connected")
+                    } else {
+                        sel.removeClass("connected")
+                    }
+                    switch (data.device.devicetype) {
+                        case DeviceType.Socket: {
+                            this.drawSwitchable(data, sel, "socket")
+                            break
+                        }
+                        case DeviceType.Switch: {
+                            this.drawSwitchable(data, sel, "switch")
+                            break
+                        }
+                        case DeviceType.Bulb: {
+                            this.drawSwitchable(data, sel, "bulb")
+                            break
+                        }
+                        case DeviceType.Thermostat: {
+                            sel.html("<img src=/public/img/thermo.svg width=30 height=30/>" + data.payload)
+                            break
+                        }
+                    }
+                    break
+                }
+            }
+        }
+    }
+
+    checkConnection(): number {
+        let count = PingInterval
+        return setInterval(() => {
+            let color = this.socket.readyState == 1 ? "lightgreen" : "red"
+            $("#wsstate").html('<svg height=25 width=25><circle cx=14 cy=18 r=5 stroke=black stroke-width=1 fill=' + color + '/></svg>')
+
+            // ping
+            count--
+            if (count == 0) {
+                this.socket.send(JSON.stringify(<DevProto>{ action: Action.Ping }))
+                count = PingInterval
+            }
+        }, 1000)
+    }
+
+    send(action: Action, id: string): void {
+        this.socket.send(JSON.stringify({
+            action: action,
+            device: { id: id }
+        } as DevProto))
+    }
+
+    setDevHdl() {
+        let instance = this
+        $(".circle").each(function (this: any) { instance.send(Action.RequestState, $(this)[0].id) })
+        $(".circle").click(function (this: any) { instance.send(Action.FlipState, $(this)[0].id) })
+    }
+
+    constructor() {
+        let socket = new ReconnectingWebSocket(wspath + '/Main/DeviceFeed')
+        this.socket = socket
+
+        // set Handler
+        socket.onmessage = this.getIncomingHdl()
+        socket.onclose = function () {
+            console.log("Websocket will be closed")
+        }
+
+        // when Document ready rendered
+        $(document).ready(() => {
+            if (socket.readyState == 1) {
+                this.setDevHdl()
             } else {
-                sel.removeClass("connected")
+                socket.onopen = () => { this.setDevHdl() }
             }
-            switch (data.device.devicetype) {
-                case DeviceType.Socket: {
-                    drawSwitchable(data, sel, "socket")
-                    break
-                }
-                case DeviceType.Switch: {
-                    drawSwitchable(data, sel, "switch")
-                    break
-                }
-                case DeviceType.Bulb: {
-                    drawSwitchable(data, sel, "bulb")
-                    break
-                }
-                case DeviceType.Thermostat: {
-                    sel.html("<img src=/public/img/thermo.svg width=30 height=30/>" + data.payload)
-                    break
-                }
-            }
-            break;
-        }
+            this.timer = this.checkConnection()
+        })
+
+        // Unload Window
+        $(window).unload(() => {
+            console.log("We leave the page")
+            socket.close()
+            window.clearInterval(this.timer)
+        })
     }
 }
 
-function checkConnection(): number {
-    let count=PingInterval
-    return setInterval(function () {
-        let color = socket.readyState == 1 ? "lightgreen" : "red"
-        $("#wsstate").html('<svg height=25 width=25><circle cx=14 cy=18 r=5 stroke=black stroke-width=1 fill=' + color + '/></svg>')
-    
-        // ping
-        count--
-        if (count==0) {
-            socket.send(JSON.stringify(<DevProto>{action:"Ping"}))
-            count=PingInterval
-        }
-    }, 1000)
-}
+new Dashboard()
 
-// main 
 
-let socket = new ReconnectingWebSocket(wspath + '/Main/DeviceFeed');
-let timer: number;
 
-socket.onmessage = hdlIncoming
-socket.onclose = function () {
-    console.log("Websocket will be closed");
-};
-
-$(document).ready(function () {
-    if (socket.readyState == 1) {
-        setDevHdl()
-    } else {
-        socket.onopen = function () { setDevHdl() }
-    }
-    timer = checkConnection()
-})
-
-$(window).unload(function () {
-    socket.close()
-    console.log("We leave the page")
-    window.clearInterval(timer)
-});
